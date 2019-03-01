b90fadada1a5a436243435bc12a6a11266b15e98
MrQuarters <andrew.zhou@uconn.edu>
2019-03-01 00:33:41 -0500

    Update sketch.js

--- advanced_web/exercise3/p5/empty-example/sketch.js
+++ advanced_web/exercise3/p5/empty-example/sketch.js
@@ -3,27 +3,30 @@ var pic = true;
 
 function preload() {
     if (pic == true) {
-    img = loadImage('img1.jpg');
+      img = loadImage('/images/img1.jpg');
     }
-else {
-img = loadImage('img2.jpg');
- }
+    else {
+      img = loadImage('/images/img2.jpg');
+    }
 }
+
 function setup() {
     var canvas = createCanvas(240, 320);
     canvas.parent('sketch-holder');
     background(255, 0, 200);
 }
+
 function draw() {
     image(img, 0, 0, width, height);
 }
+
 function mousePressed() {
     if (pic == true) {
-        img = loadImage('img1.jpg');
+        img = loadImage('/images/img1.jpg');
         pic = false;
     }
     else {
-        img = loadImage('img2.jpg');
+        img = loadImage('/images/img2.jpg');
         pic = true;
     }
 }

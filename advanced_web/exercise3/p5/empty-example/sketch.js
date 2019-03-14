1513a306b0dfada275696d9825c22ecbfe5c2a7e
MrQuarters <andrew.zhou@uconn.edu>
2019-03-14 11:25:03 -0400

    Update sketch.js

--- advanced_web/exercise3/p5/empty-example/sketch.js
+++ advanced_web/exercise3/p5/empty-example/sketch.js
@@ -1,28 +1,28 @@
 let image;
 let click = true;
 
-function preload() {
-      image = loadImage('/images/img1.jpg');
-      image = loadImage('/images/img2.jpg');
-}
+//function preload() {
+//      image = loadImage('/images/img1.jpg');
+//      image = loadImage('/images/img2.jpg');
+//}
 
 function setup() {
     var canvas = createCanvas(240, 320);
     canvas.parent('pictures');
-    background(255);
+    background(125);
 }
 
 function draw() {
-    image(image, 0, 0, width, height);
+  //  image(image, 0, 0, width, height);
 }
 
-function mousePressed() {
-    if (click = true) {
-        image = loadImage('/images/img1.jpg');
-        click = false;
-    }
-    else if (click = false) {
-        image = loadImage('/images/img2.jpg');
-        click = true;
-    }
-}
+//function mousePressed() {
+//    if (click = true) {
+//        image = loadImage('/images/img1.jpg');
+//        click = false;
+//    }
+//    else if (click = false) {
+//        image = loadImage('/images/img2.jpg');
+//        click = true;
+//    }
+//}

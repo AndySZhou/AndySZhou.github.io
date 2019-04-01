0bdfceb4458d61534f5f551c0fd48fb9c66b808d
MrQuarters <andrew.zhou@uconn.edu>
2019-04-01 14:52:13 -0400

    Update sketch.js

--- advanced_web/exercise3/p5/empty-example/sketch.js
+++ advanced_web/exercise3/p5/empty-example/sketch.js
@@ -1,4 +1,4 @@
-let pic;
+/* let pic;
 let click = true;
 
 //function preload() {
@@ -27,4 +27,17 @@ function mousePressed() {
 
 function draw() {
     image(pic, 0, 0, 240, 320);
+}
+*/
+let value = 0;
+function draw() {
+  fill(value);
+  rect(25, 25, 50, 50);
+}
+function mousePressed() {
+  if (value === 0) {
+    value = 255;
+  } else {
+    value = 0;
+  }
 }

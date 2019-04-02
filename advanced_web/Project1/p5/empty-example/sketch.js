751fe09af1a33d8dfede77d213a15f0af0ad1326
MrQuarters <andrew.zhou@uconn.edu>
2019-04-02 03:48:10 -0400

    Interactive Map

--- advanced_web/Project1/p5/empty-example/sketch.js
+++ advanced_web/Project1/p5/empty-example/sketch.js
@@ -1,50 +1,38 @@
-var img;
-var pic = true;
-
-function preload() {
-    if (pic == true) {
-    img = loadImage('img1.JPG'); // load media and other data
-    }
-else {
-img = loadImage('img2.JPG'); // load media and other data
- }
-}
+let pic;
+let loc1 = "Scofieldtown Road, the location of the second UConn Stamford Campus.";
+let loc2 = "The current building where UConn Stamford is located.";
+let loc3 = "Stamford High School, where UConn Stamford was first based.";
+//function preload() {
+//      image = loadImage('images/img1.JPG');
+//      image = loadImage('/images/img2.jpg');
+//}
 
 function setup() {
-    var canvas = createCanvas(windowWidth, windowHeight);
-
-    // Move the canvas so it’s inside our <div id="sketch-holder">.
-    canvas.parent('sketch-holder');
-
-    background(255, 0, 200);
+  var cnv = createCanvas(1209, 719);
+  var x = (windowWidth - width) / 2;
+  var y = (windowHeight - height) / 2;
+  cnv.position(x, y);
+  background(255, 0, 200);
+  pic = loadImage('images/StamMap.JPG');
 }
-
-
-function windowResized() {
-    resizeCanvas(windowWidth, windowHeight);
-}
-
-/*
 function draw() {
-    if (mouseIsPressed) {
-        fill(0);
-    } else {
-        fill(0);
+    image(pic, 0, 0, 1297, 719);
+    //ellipse(638, 55, 20, 20);
+    //ellipse(740, 655, 20, 20);
+    //ellipse(793, 650, 20, 20);
+    if(mouseX > 628 && mouseX < 648) {
+      if(mouseY > 45 && mouseY <65) {
+        text(loc1, 648, 65, 668, 85);
+      }
     }
-    ellipse(mouseX, mouseY, 80, 80);
-} */
-
-function draw() { // only executed once preload is has finished loading data
-    image(img, 0, 0, width, height);
+    if(mouseX > 730 && mouseX < 750) {
+      if(mouseY > 645 && mouseY <665) {
+        text(loc2, 730, 665, 780, 655);
+      }
+    }
+    if(mouseX > 783 && mouseX < 803) {
+      if(mouseY > 640 && mouseY <660) {
+        text(loc3, 783, 660, 803, 680);
+      }
+    }
 }
-
-// global mousePressed handler
-function mousePressed() {
-    /* img = loadImage('day.jpg'); // load media and other data */
-    if (pic == true) {
-        img = loadImage('img2.jpg'); // load media and other data
-        pic = false;
-    }
-    else {
-        img = loadImage('img1.jpg'); // load media and other data
-        pic = true;

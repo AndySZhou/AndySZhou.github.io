49740f70f69f4d853ca7ac680f62b6e2c2d201dd
MrQuarters <andrew.zhou@uconn.edu>
2019-04-03 23:56:39 -0400

    Update sketch.js

--- advanced_web/Project1/p5/empty-example/sketch.js
+++ advanced_web/Project1/p5/empty-example/sketch.js
@@ -2,10 +2,9 @@ let pic;
 let loc1 = "Scofieldtown Road, the location of the second UConn Stamford Campus.";
 let loc2 = "The current building where UConn Stamford is located.";
 let loc3 = "Stamford High School, where UConn Stamford was first based.";
-//function preload() {
-//      image = loadImage('images/img1.JPG');
-//      image = loadImage('/images/img2.jpg');
-//}
+function preload() {
+  pic = loadImage('images/StamMap.JPG');
+}
 
 function setup() {
   var cnv = createCanvas(1209, 719);
@@ -13,7 +12,6 @@ function setup() {
   var y = (windowHeight - height) / 2;
   cnv.position(x, y);
   background(255, 0, 200);
-  pic = loadImage('images/StamMap.JPG');
 }
 function draw() {
     image(pic, 0, 0, 1297, 719);
